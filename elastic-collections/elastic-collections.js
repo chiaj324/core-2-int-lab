3139ae5109a0c8a8f126aed87dc33f356f13e9ca
chiaj324 <chiaj324@newschool.edu>
2023-03-23 18:35:28 -0400

    3/23

--- elastic-collections/elastic-collections.js
+++ elastic-collections/elastic-collections.js
@@ -14,14 +14,17 @@ fetch('elastic-collections.json')
       let newItem = document.createElement("div");
       newItem.classList.add("icon");
       newItem.innerHTML = `
-        <div class="title">${item.title}</div>
+        <div class="image height-${item.class}"><img src="assets/imgs/${item.image}"></div>
         <div class="year">${item.year}</div>
+        <div class="title">${item.title}</div>
         <div class="director">${item.director}</div>
         <div class="budget">${item.budget}</div>
         <div class="boxoffice">${item.boxoffice}</div>
-        <div class="rottentomatoes">${item.rottentomatoes}</div>
-        <div class="image"><img src="assets/imgs/${item.image}"></div>
-        <div class="class">${item.class}</div>`;
+        <div class="rottentomatoes">${item.rottentomatoes}</div>`;
       container.appendChild(newItem);    
     });
+    container.appendChild(newItem); 
+    newItem.addEventListener('click', function(){
+      newItem.classList.toggle("active");
+    });   
 }
